109f4e24b43b3e847058b5afa1fe2f7001f0a0cf
piSINUS <timofejsolovov70@gmail.com>
2022-12-17 00:35:02 +0300

    transactions

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,9 +10,21 @@ class HomeController extends Controller
 
     public function index()
     {
-        // $query= DB::insert("INSERT INTO posts(title,content) VALUES(:title,:content )",['content'=>'State cintent 5','title'=>'State 5']);
-        DB::update('UPDATE posts Set created_at=?,updated_at=? Where created_at Is Null Or updated_at Is Null',[ Now(), Now()]);
-        $posts= DB::select("SELECT * From posts Where id > :id",['id'=>0]);
+        // $query= DB::insert("INSERT INTO posts(title,content) VALUES(:title,:content )",['content'=>'State cintent 5','title'=>'State 5']);Добавление
+        // DB::update('UPDATE posts Set created_at=?,updated_at=? Where created_at Is Null Or updated_at Is Null',[ Now(), Now()]); Обновление
+        // DB::delete("DELETE FROM posts Where id > :id",['id'=>5]);
+        DB::beginTransaction();//Транзакции тип оброботка ошибки
+        try {
+            DB::update("UPDATE posts SET created_at = ? WHERE created_at IS NULL", [NOW()]);
+            DB::update("UPDATE posts SET updated_at = ? WHERE updated_at IS NULL", [NOW()]);
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            echo $e->getMessage();
+        }
+        
+        
+        $posts= DB::select("SELECT * From posts Where id > :id",['id'=>0]);//Получение
         return $posts;
         // dump($_ENV['MY_SETTING']);
         // dump(env('MY_SETTING2'));
