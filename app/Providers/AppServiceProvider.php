50e20f45581b2af6c9bcaf806f3326048366522a
piSINUS <timofejsolovov70@gmail.com>
2022-12-23 21:00:36 +0300

    ad

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        DB::listen(function ($query)){
+            dump($query->sql,$query->bindings);
+        }
     }
 }
